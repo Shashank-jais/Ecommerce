fddabb03ae3a2f21a079025468e7f336f20d1b9b
Shashank-jais <shashankjaiswal266@gmail.com>
2024-06-28 13:07:22 +0530

    final commit

--- Frontend/src/components/footer.js
+++ Frontend/src/components/footer.js
@@ -3,7 +3,9 @@ import React from 'react'
 const footer = () => {
   return (
     <footer className='bg-slate-300'>
-      <div className='container flex items-center justify-center p-4'><p className='font-bold  ' >Awesome Coding!</p></div>
+      <div className='container flex items-center justify-center p-4'><p className='font-bold  ' >Awesome Coding!</p>
+      <p className='font-bold absolute right-8 '>Created by : Shashank Jasiwal</p>
+      </div>
     </footer>
   )
 }
