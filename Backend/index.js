afedd91e6eb683b96e0487774c0c7d94dd16bd39
Shashank-jais <shashankjaiswal266@gmail.com>
2024-06-28 11:59:23 +0530

    fixing

--- Backend/index.js
+++ Backend/index.js
@@ -7,12 +7,12 @@ var cookieParser = require('cookie-parser')
 
 
 const app = express();
-app.use(cookieParser())
 app.use(cors({
     origin: 'https://ecommerce-beta-beige-77.vercel.app',
     credentials: true
 }))
 app.use(express.json());
+app.use(cookieParser())
 app.use("/api", router)
 const port = 8080 || process.env.PORT
 connectDB().then(() => {
