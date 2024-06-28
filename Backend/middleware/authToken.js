50bca5448a97d48f18eb71d1e4afc2bfef3dd583
Shashank-jais <shashankjaiswal266@gmail.com>
2024-06-28 12:25:11 +0530

    debug

--- Backend/middleware/authToken.js
+++ Backend/middleware/authToken.js
@@ -1,37 +1,41 @@
-const jwt = require('jsonwebtoken');
+const jwt = require('jsonwebtoken')
 
-async function authToken(req, res, next) {
-    try {
-        const token = req.cookies?.token;
-        if (!token) {
-            return res.status(401).json({
-                message: 'Please Login ...',
-                error: true,
-                success: false
-            });
+async function authToken(req,res,next){
+    try{
+        const token = req.cookies?.token
+
+        console.log("token",token)
+        if(!token){
+            return res.status(200).json({
+                message : "Please Login...!",
+                error : true,
+                success : false
+            })
         }
 
-        jwt.verify(token, process.env.TOKEN_SECRET_KEY, function (err, decoded) {
-            if (err) {
-                console.error('JWT verification error:', err);
-                return res.status(401).json({
-                    message: 'Unauthorized',
-                    error: true,
-                    success: false
-                });
+        jwt.verify(token, process.env.TOKEN_SECRET_KEY, function(err, decoded) {
+            console.log(err)
+            console.log("decoded",decoded)
+            
+            if(err){
+                console.log("error auth", err)
             }
-            req.userId = decoded?._id; // Assuming your decoded token has _id field
-            next();
+
+            req.userId = decoded?._id
+
+            next()
         });
 
-    } catch (err) {
-        console.error('Error in authToken middleware:', err);
+
+    }catch(err){
         res.status(400).json({
-            message: err.message || 'Unexpected error',
-            error: true,
-            success: false
-        });
+            message : err.message || err,
+            data : [],
+            error : true,
+            success : false
+        })
     }
 }
 
-module.exports = authToken;
+
+module.exports = authToken
